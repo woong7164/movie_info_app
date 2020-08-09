3a794942d69be268f595f233e924ddaac6e9b558
woong7164 <woong7164@gmail.com>
2020-08-09 23:00:02 +0900

    라우트 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,67 +1,18 @@
 import React from "react";
-import axios from "axios";
-
-import Movies from "./Movies";
-import "./app.css";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Navigation from "./components/Navigation";
 
 //https://yts-proxy.now.sh/list_movies.json
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: []
-  };
-
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies }
-      }
-    } = await axios.get(
-      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
-    );
-    console.log("data", movies);
-    this.setState({
-      isLoading: false,
-      movies: movies
-    });
-    console.log("movie", this.state.movies);
-  };
-  componentDidMount() {
-    this.getMovies();
-  }
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map(movie => {
-              console.log("movie", movie);
-              return (
-                <Movies
-                  key={movie.id}
-                  id={movie.id}
-                  year={movie.year}
-                  title={movie.title}
-                  summary={movie.summary}
-                  poster={movie.medium_cover_image}
-                  genres={movie.genres}
-                />
-              );
-            })}
-          </div>
-        )}
-      </section>
-    );
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+  );
 }
 
 export default App;
-
-// export default function App() {
-//   return <div>dddddd</div>;
-// }
---
+++ src/routes/About.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+//https://yts-proxy.now.sh/list_movies.json
+function About() {
+  return <span>about this page</span>;
+}
+
+export default About;
